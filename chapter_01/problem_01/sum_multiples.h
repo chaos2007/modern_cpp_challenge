b59501697e86d43c3a5b8a7edba74e72f79eef85
Don Pollitz <chaos2007@gmail.com>
2018-06-15 10:35:47 -0500

    Add template project structure for projects.

---
+++ chapter_01/problem_01/sum_multiples.h
@@ -0,0 +1,11 @@
+#ifndef CPPCHALLENGER_SUM_MULTIPLES_H
+#define CPPCHALLENGER_SUM_MULTIPLES_H
+
+
+int sum_multiples_of_3_and_5(int max_number)
+{
+    return 0;
+}
+
+
+#endif //CPPCHALLENGER_SUM_MULTIPLES_H
---
+++ chapter_01/problem_01/test_main.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "sum_multiples.h"
+
+TEST_CASE( "sum_multiples wors", "[sum_multiples]" ) {
+    REQUIRE( sum_multiples_of_3_and_5(3) == 3 );
+    REQUIRE( sum_multiples_of_3_and_5(5) == 8 );
+    REQUIRE( sum_multiples_of_3_and_5(6) == 14 );
+}
