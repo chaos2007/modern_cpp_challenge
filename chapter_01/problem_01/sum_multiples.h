44a2482e3d1cbafd4f00d7f2017c5f2848105ac6
Don Pollitz <chaos2007@gmail.com>
2018-06-15 13:33:44 -0500

    Add initial impl of the sum of multiples.

--- chapter_01/problem_01/sum_multiples.h
+++ chapter_01/problem_01/sum_multiples.h
@@ -1,10 +1,13 @@
 #ifndef CPPCHALLENGER_SUM_MULTIPLES_H
 #define CPPCHALLENGER_SUM_MULTIPLES_H
 
+#include <boost/range/irange.hpp>
 
-int sum_multiples_of_3_and_5(int max_number)
+auto sum_multiples_of_3_and_5(auto max_number)
 {
-    return 0;
+    auto num_range = boost::irange(3, max_number + 1);
+    auto add_multiples = [](auto acc, auto num) {return (num % 3 == 0 || num % 5 == 0) ? acc + num : acc;};
+    return std::accumulate(num_range.begin(), num_range.end(), 0, add_multiples);
 }
 
 
