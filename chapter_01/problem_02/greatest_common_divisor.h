03427f5aae131a6186ccb38f77ff20c3d5b033cb
Don Pollitz <chaos2007@gmail.com>
2018-06-21 08:02:03 -0500

    Add problem_02.

--- chapter_01/problem_02/greatest_common_divisor.h
+++ chapter_01/problem_02/greatest_common_divisor.h
@@ -3,7 +3,13 @@
 
 int greatest_common_divisor(int num_1, int num_2)
 {
-    return 0;
+    if( num_2 == 0 ) {
+        return num_1;
+    }
+    if( num_1 == 0 ) {
+        return num_2;
+    }
+    return greatest_common_divisor(num_2, num_1 % num_2);
 }
 
 #endif //CPPCHALLENGER_GREATEST_COMMON_DIVISOR_H
--- chapter_01/problem_02/main.cpp
+++ chapter_01/problem_02/main.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+#include "greatest_common_divisor.h"
+
 int main()
 {
+    int num_1, num_2;
+    std::cout << "Give me two numbers to determine common divisor? ";
+    std::cin >> num_1 >> num_2;
+    auto result = greatest_common_divisor(num_1, num_2);
+    std::cout << result;
     return 0;
 }
