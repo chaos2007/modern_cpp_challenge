18e4c9c305026d04f0f13a0ce554e121685032c7
Don Pollitz <chaos2007@gmail.com>
2018-08-06 14:52:28 +0000

    Lower CMake version and and problem 6 template

---
+++ chapter_01/problem_06/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "my_math_lib.h"
+
+int main()
+{
+    return 0;
+}
---
+++ chapter_01/problem_06/test_main.cpp
@@ -0,0 +1,18 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "my_math_lib.h"
+
+TEST_CASE( "is_prime works", "[prime]" ) {
+    REQUIRE( is_prime(1) );
+    REQUIRE( is_prime(2) );
+    REQUIRE( is_prime(3) );
+    REQUIRE( !is_prime(4) );
+    REQUIRE( is_prime(5) );
+    REQUIRE( !is_prime(6) );
+    REQUIRE( is_prime(7) );
+}
+
+TEST_CASE( "largest_prime_under works", "[prime]") {
+    REQUIRE( largest_prime_below( 10 ) == 7 );
+    REQUIRE( largest_prime_below( 100) == 97 );
+}
